4177ca8a2c5d7c91709424dd9ca2339c28a34d78
William Pépin <1462346@etu.cegep-ste-foy.qc.ca>
2017-02-03 09:07:01 -0500

    config.php connexion a la bd

--- public_html/public/programme/equipe/index.php
+++ public_html/public/programme/equipe/index.php
@@ -5,3 +5,24 @@
  * Date: 17-01-25
  * Time: 08:51
  */
+$niveau = "../../";
+include_once($niveau . 'inc/lib/Twig/Autoloader.php');
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem($niveau . 'templates'); //Nom du dossier qui contient nos templates
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+    'debug' => true
+));
+
+
+///////////// EXEMPLE AVEC TWIG //////////////
+$template = $twig->loadTemplate('pieces/menu.html.twig');
+
+$template = $twig->loadTemplate('programme/equipe/index.html.twig');
+echo $template->render(array(
+    'niveau' => "../",
+    'title' => "Techniques d'intégration multimédia | TIM"
+));
+
+$template = $twig->loadTemplate('pieces/footer.html.twig');
+
