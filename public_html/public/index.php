2151619b99aa1133f802f44ddb37b878af14ae8b
Vincent Béland <1231359@etu.cegep-ste-foy.qc.ca>
2017-02-03 08:24:43 -0500

    V - exemple avec twig

--- public_html/public/index.php
+++ public_html/public/index.php
@@ -2,4 +2,23 @@
 
 include_once('inc/lib/Twig/Autoloader.php');
 Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('templates'); //Nom du dossier qui contient nos templates
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+    'debug' => true
+));
+
+
+
+///////////// EXEMPLE AVEC TWIG //////////////
+$template = $twig->loadTemplate('pieces/menu.html.twig');
+
+$template = $twig->loadTemplate('index.html.twig');
+echo $template->render(array(
+    'niveau' => "../",
+    'will' => "the dog",
+    'title' => "Techniques d'intégration multimédia | TIM"
+));
+
+$template = $twig->loadTemplate('pieces/footer.html.twig');
 
