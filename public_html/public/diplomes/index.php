eb49fef32b9c4b02836d2173c96f1bacbccc858b
Annabelle Violette <anna.violette@hotmail.com>
2017-02-03 10:45:30 -0500

    A - correction head title

--- public_html/public/diplomes/index.php
+++ public_html/public/diplomes/index.php
@@ -17,14 +17,32 @@ $strTriInterets = "";
 /*************** INSTANCIATION CONFIG ET TWIG ***********************/
 require_once($strNiveau . 'inc/scripts/fctcommunes.inc.php');
 
+/*************** REQUÊTES DIPLÔMÉS ***********************/
 
+//-----Requete pour aller chercher les infos sur le livre-----//
+$strSQLDiplomes = "SELECT nom_diplome, slug FROM t_diplome ORDER BY nom_diplome";
+if ($objResultDiplome = $objConnMySQLi->query($strSQLDiplomes)) {
+    while ($objLigneDiplome = $objResultDiplome->fetch_object()) {
+        $arrDiplomes[] =
+            array(
+                'nom'=>$objLigneDiplome->nom_diplome,
+                'slug'=>$objLigneDiplome->slug
+            );
+    }
+}
+//En cas d'erreur de requête
+if($objResultDiplome->num_rows == 0){
+    header('Location: ' . $strNiveau . 'erreur/index.php');
+}
+
+$objResultDiplome->free_result();
 
 
 ///////////// TWIG //////////////
 $template = $twig->loadTemplate('pieces/head.html.twig');
 echo $template->render(array(
     'title' => "Techniques d'intégration multimédia | TIM",
-    'specifique' => "Nos diplômés 2017"
+    'page' => " | Nos diplômés 2017"
 ));
 $template = $twig->loadTemplate('pieces/header.html.twig');
 echo $template->render(array(
@@ -33,8 +51,10 @@ echo $template->render(array(
 $template = $twig->loadTemplate('diplomes/index.html.twig');
 echo $template->render(array(
     'niveau' => "../",
-    'will' => "the dog"
+    'page' => "Nos diplômés 2017",
+    'diplomes' => $arrDiplomes
 ));
+print_r($arrDiplomes);
 
 $template = $twig->loadTemplate('pieces/footer.html.twig');
 echo $template->render(array());
--- public_html/public/index.php
+++ public_html/public/index.php
@@ -7,6 +7,12 @@ require_once($strNiveau . 'inc/scripts/fctcommunes.inc.php');
 
 
 ///////////// EXEMPLE AVEC TWIG //////////////
+$template = $twig->loadTemplate('pieces/head.html.twig');
+echo $template->render(array(
+    'title' => "Techniques d'intégration multimédia | TIM",
+    'page' => ""
+));
+
 $template = $twig->loadTemplate('pieces/header.html.twig');
 
 $template = $twig->loadTemplate('index.html.twig');
