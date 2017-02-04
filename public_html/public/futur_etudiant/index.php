d86e634fcbbf73d445d43c862b0fe442dd739b14
William Pépin <1462346@etu.cegep-ste-foy.qc.ca>
2017-02-03 19:08:26 -0500

    requetes de toutes mes pages

--- public_html/public/futur_etudiant/index.php
+++ public_html/public/futur_etudiant/index.php
@@ -3,5 +3,47 @@
  * Created by PhpStorm.
  * User: vincentbeland
  * Date: 17-01-25
- * Time: 08:48
+ * Time: 08:51
  */
+$strNiveau = "../";
+require_once($strNiveau . 'inc/scripts/fctcommunes.inc.php');
+
+
+$strSQLTextePageFuturEtudiant = "SELECT titre_texte, texte FROM t_texte WHERE section_et_page = 'Futur étudiant'";
+
+if ($objResultTexte = $objConnMySQLi->query($strSQLTextePageFuturEtudiant)) {
+
+    while ($objLigneTexte = $objResultTexte->fetch_object()) {
+
+        $arrTextes[]=
+
+            array(
+                'titre'=>$objLigneTexte->titre_texte,
+                'paragraphe'=>$objLigneTexte->texte
+            );
+
+    }
+    $objResultTexte->free_result();
+}
+
+$objConnMySQLi->close();
+
+
+$template = $twig->loadTemplate('pieces/head.html.twig');
+echo $template->render(array(
+    'title' => "Techniques d'intégration multimédia | TIM",
+    'page' => "Perspectives | "
+));
+
+$template = $twig->loadTemplate('pieces/header.html.twig');
+echo $template->render(array());
+
+$template = $twig->loadTemplate('futur_etudiant/index.html.twig');
+echo $template->render(array(
+    'niveau' => "../",
+    'arrTextes' => $arrTextes
+));
+
+$template = $twig->loadTemplate('pieces/footer.html.twig');
+echo $template->render(array());
+
--- public_html/public/programme/index.php
+++ public_html/public/programme/index.php
@@ -5,3 +5,45 @@
  * Date: 17-01-25
  * Time: 08:51
  */
+$strNiveau = "../";
+require_once($strNiveau . 'inc/scripts/fctcommunes.inc.php');
+
+
+$strSQLTextePageProgramme = "SELECT titre_texte, texte FROM t_texte WHERE section_et_page = 'Programme - Page d''entrée de la section'";
+
+if ($objResultTexte = $objConnMySQLi->query($strSQLTextePageProgramme)) {
+
+    while ($objLigneTexte = $objResultTexte->fetch_object()) {
+
+        $arrTextes[]=
+
+            array(
+                'titre'=>$objLigneTexte->titre_texte,
+                'paragraphe'=>$objLigneTexte->texte
+            );
+
+    }
+    $objResultTexte->free_result();
+}
+
+$objConnMySQLi->close();
+
+
+$template = $twig->loadTemplate('pieces/head.html.twig');
+echo $template->render(array(
+    'title' => "Techniques d'intégration multimédia | TIM",
+    'page' => "Programme | "
+));
+
+$template = $twig->loadTemplate('pieces/header.html.twig');
+echo $template->render(array());
+
+$template = $twig->loadTemplate('programme/index.html.twig');
+echo $template->render(array(
+    'niveau' => "../",
+    'arrTextes' => $arrTextes
+));
+
+$template = $twig->loadTemplate('pieces/footer.html.twig');
+echo $template->render(array());
+
