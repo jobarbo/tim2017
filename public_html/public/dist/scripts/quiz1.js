4a9f6fb692e4f6673b402c46d4cedacdb9764d7d
Vincent Béland <vincent64ify@gmail.com>
2017-02-14 14:53:18 -0500

    V - prototype quiz profil

--- public_html/public/dist/scripts/quiz1.js
+++ public_html/public/dist/scripts/quiz1.js
@@ -2,43 +2,36 @@
  * Created by vincentbeland on 17-02-14.
  */
 var app = $(function configurer(evenement){
-
     var questionActive = 0;
     var pointage = 0;
-    /* Configuration: ajouts des écouteurs d'événements et état initial de l'app */
-    reInitQuestion(questionActive);
-
-    $( "fieldset" ).each(function( index ) {
-        $("#Q" + index).hide();
-    });
+    $('#validerQuiz').on("click", validerMonChoix);
+    for (var i = 1; i <= 9; i++) {
+        $("#Q" + i).hide();
+    }
 
     /**
      * @param {int} no  -> le numéro de la question à afficher
      */
-
     function reInitQuestion(no){
-
+        console.log(no);
+        $("#Q" + (no - 1)).hide();
+        $("#Q" + (no)).show();
     }
 
     function validerMonChoix(evenement){
         evenement.preventDefault();
+        allerProchaineQuestion(evenement);
     }
+
     function allerProchaineQuestion(evenement)
     {
         evenement.preventDefault();
+        pointage = pointage + parseInt($("input[name=Q"+ questionActive + "]:checked").val());
         questionActive++;
         reInitQuestion(questionActive)
+        if (questionActive == 10) {
+            $('#validerQuiz').remove();
+            console.log(pointage * 2);
+        }
     }
-
-    /* Autres fonctions? ... */
-
-    function commencerQuiz(evenement)
-    {
-        evenement.preventDefault();
-        $('#commencer').remove();
-        questionActive++;
-        $('#Q0').hide();
-        reInitQuestion(questionActive);
-    }
-
 });
