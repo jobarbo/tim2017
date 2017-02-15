50d831891ae4fc27bcb3d591f35917e86d9774a3
Annabelle Violette <anna.violette@hotmail.com>
2017-02-15 12:21:06 -0500

    A - ajout des images des projets + delete config pour le gitignore et afficher images des projets dans la fiche de l'étudiant et du projet

--- public_html/public/nous_joindre/index.php
+++ public_html/public/nous_joindre/index.php
@@ -46,11 +46,10 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 
-//Templates
 $template = $twig->loadTemplate('pieces/head.html.twig');
 echo $template->render(array(
     'title' => "Techniques d'intégration multimédia | TIM",
-    'page' => "Les Stages en Techniques d'Intégration Multimédia",
+    'page' => "Nous joindre | ",
     'niveau' => $strNiveau
 ));
 
