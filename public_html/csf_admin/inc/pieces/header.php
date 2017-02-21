c2849b4d5a67a1b8689274552d386f4a1303d1e0
William <williamcharestpepin@gmail.com>
2017-02-21 11:08:11 -0500

    admindd

--- public_html/csf_admin/inc/pieces/header.php
+++ public_html/csf_admin/inc/pieces/header.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: annabelleViolette
- * Date: 17-02-07
- */
+ * @author William Charest-Pépin <williamcharestpepin@gmail.com>
+ * @copyright Copyright (c)2017 – Cégep de sainte-Foy
+ * Date: 2017-02
+ *
+*/
+
---
+++ public_html/csf_admin/routeur.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+/*************** 1. VARIABLES LOCALES ***********************/
+$strNiveau = "";
+$strNiveauAdmin="../public/";
+
+
+/*************** 2. INSTANCIATION CONFIG ET TWIG ***********************/
+require_once($strNiveau . 'inc/scripts/fctcommunes.inc.php');
+
+if(isset($_SESSION['arrAuthentification'])){
+
+    $arrAuthentification = unserialize($_SESSION['arrAuthentification']);
+
+    switch($arrAuthentification["niveau_acces"]){
+
+        case 1:
+            header('Location: fiche_etudiant/index.php?id=' . $arrAuthentification["nom_usager_admin"]);
+            exit;
+            break;
+
+        case 3:
+            header('Location: index.php');
+            exit;
+            break;
+
+    }
+}
