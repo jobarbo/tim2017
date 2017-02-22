46f2899e93e1ff85765b1dd13db9f1c442fd878d
Vincent Béland <vincent64ify@gmail.com>
2017-02-21 20:23:12 -0500

    V - quizs fonctionnels

--- public_html/public/assets/scripts/quiz2.js
+++ public_html/public/assets/scripts/quiz2.js
@@ -3,42 +3,115 @@
  */
 var app = $(function configurer(evenement){
 
+    $('#validerQuiz').hide();
     var questionActive = 0;
     $("#progression").text("1/5");
-    $('#validerQuiz').on("click", validerMonChoix);
     for (var i = 1; i <= 5; i++) {
         $("#Q" + i).hide();
         $("#qProfil" + i).hide();
     }
+
+    /* Configuration: ajouts des écouteurs d'événements et état initial de l'app */
+    var $qo = "";
+    var $qn = "";
+
+    var $q0o = "Oui, Tim te permettra d’apprendre à concevoir et réaliser des éléments visuels et sonores de toutes sortes à l’aide de logiciels professionnels, sur plateforme Mac et PC avec par exemple la suite professionnelle d’Adobe.";
+    var $q0n = "Attention, le programme Tim comporte un important pourcentage de cours dédiés à la conception et réalisation d’éléments visuels et sonores de toutes sortes à l’aide de logiciels professionnels, sur plateforme Mac et PC avec par exemple la suite professionnelle d’Adobe.";
+    var $q1o = "Oui, Tim te permettra d'apprendre à programmer des pages Web et des produits interactifs à l'aide de langages de programmation. Les langages de programmation utilisés sont par exemple : JavaScript et PHP.";
+    var $q1n = "Attention, le programme Tim comporte un important pourcentage de cours dédiés à la programmation de pages web et de produits interactifs. Les langages de programmation utilisés sont par exemple : JavaScript et PHP.<";
+    var $q2o = "Non, Tim ne te permettra pas d’apprendre des notions reliées au domaine de la télédiffusion. Tout au plus, tu auras un cours de 60 heures dédié à la captation et au montage audiovidéo au cours de tes trois années de formation. Il existe des programmes spécialisés dans le domaine des communications, par exemple : http://www.cegepjonquiere.ca/arts-lettres-et-communication.html";
+    var $q2n = "En effet, TIM ne te permettra pas d’apprendre des notions reliées au domaine de la télédiffusion. Tout au plus, tu auras un cours de 60 heures dédié à la captation et au montage audio-vidéo au cours de tes trois années de formation.";
+    var $q3o = "Non, très peu. Tout au plus, tu auras un cours de 45 heures dédié à l'apprentissage de l'animation dans un contexte de diffusion pour le Web. Il existe des programmes spécialisés dans le domaine du 3D, par exemple: http://www.cegeplimoilou.ca/formations/diplomes-d-etudes-collegiales-dec-technique/574b0-animation-3d-et-synthese-d-images/";
+    var $q3n = "En effet, très peu. Tout au plus, tu auras un cours de 45 heures dédié à l'apprentissage de l'animation dans un contexte de diffusion pour le Web.";
+    var $q4o = "Non, Tim ne te permettra pas de développer des compétences en lien avec l’imprimé et l’illustration. Il existe des programmes spécialisés dans le domaine du Graphisme, par exemple : http://www.cegep-ste-foy.qc.ca/programmes/programmes-techniques/graphisme/";
+    var $q4n = "En effet, Tim ne te permettra pas de développer des compétences en lien avec l’imprimé et l’illustration. Il existe des programmes spécialisés dans le domaine du Graphisme, par exemple : http://www.cegep-ste-foy.qc.ca/programmes/programmes-techniques/graphisme/";
+
+    reInitQuestion(questionActive);
     /**
      * @param {int} no  -> le numéro de la question à afficher
      */
-    function reInitQuestion(no){
+    function reInitQuestion(no) {
+        switch (no)
+        {
+            case 0:
+                $qo = $q0o;
+                $qn = $q0n;
+                break;
+            case 1:
+                $qo = $q1o;
+                $qn = $q1n;
+                break;
+            case 2:
+                $qo = $q2o;
+                $qn = $q2n;
+                break;
+            case 3:
+                $qo = $q3o;
+                $qn = $q3n;
+                break;
+            case 4:
+                $qo = $q4o;
+                $qn = $q4n;
+                break;
+        }
+
         console.log(no);
+        if (no > 0)
+        {
+            $("#progression").text((no + 1).toString() + "/5");
+        }
+        $("#explication").text("");
+        $("#explication").removeClass();
         $("#Q" + (no - 1)).hide();
         $("#Q" + (no)).show();
         $("#qProfil" + (no - 1)).hide();
         $("#qProfil" + (no)).show();
+        $('#Q' + no).after("<input id='validerQuestion' name='validerQuestion' type='submit' value='Valider mon choix' />");
+        $('#validerQuestion').on("click", validerMonChoix);
     }
 
     function validerMonChoix(evenement)
     {
-        if($("input[name=Q"+ questionActive + "]:checked").val() === undefined) {
+        $("#retroaction").text("");
+        evenement.preventDefault();
+        if($("input[name=Q"+ questionActive + "]:checked").val() === undefined)
+        {
             $("#retroaction").text("Veuillez sélectionner une réponse.");
-            evenement.preventDefault();
-        } else {
+        }
+        else
+        {
+            if($("input[name=Q"+ questionActive + "]:checked").val() == "Oui")
+            {
+                $("#explication").text($qo);
+                $("#explication").addClass("bon");
+            }
+            else
+            {
+                $("#explication").text($qn);
+                $("#explication").addClass("mauvais");
+            }
 
+            $("input[name=Q" + questionActive + "]").attr('disabled', 'disabled');
+            $('#validerQuestion').remove();
+
+            if (questionActive < 4)
+            {
+                $('#Q' + questionActive).after("<input id='questionSuivante' name='questionSuivante' type='submit' value='Question suivante' />");
+                $('#questionSuivante').on("click", allerProchaineQuestion);
+            }
+            else
+            {
+                $("input[type='radio']").removeAttr('disabled');
+                $('#validerQuiz').show();
+            }
         }
     }
 
     function allerProchaineQuestion(evenement)
     {
-        function allerProchaineQuestion(evenement)
-        {
-            evenement.preventDefault();
-            $('#questionSuivante').remove();
-            reInitQuestion(questionActive);
-        }
+        evenement.preventDefault();
+        questionActive++;
+        $('#questionSuivante').remove();
+        reInitQuestion(questionActive);
     }
-
 });
--- public_html/public/futur_etudiant/bon_choix/index.php
+++ public_html/public/futur_etudiant/bon_choix/index.php
@@ -41,15 +41,76 @@ foreach($quiz->questions->question as $questions)
     $cptQ++;
 }
 
-$arrId = ["A", "B"];
+$arrId = array("A", "B");
 
-///////////// TWIG //////////////
-$template = $twig->loadTemplate('futur_etudiant/bon_choix/index.html.twig');
-echo $template->render(array(
-    'niveau' => $strNiveau,
-    'arrMenuLiensActifs' => $arrMenuActif,
-    'page' => "Fais-tu le bon choix? | ",
-    'xml' => $arrQuestions,
-    'tId' => $arrId,
-    'fichier_script' => 'quiz2.js'
-));
+$erreur = false;
+if(isset($_GET['validerQuiz']))
+{
+    //////////////////// QUIZ COMPLÉTÉ ////////////////////
+    for($cpt = 0; $cpt <= 4; $cpt++)
+    {
+        if(isset($_GET['Q'.$cpt]))
+        {
+            $erreur = false;
+        }
+        else
+        {
+            $erreur = true;
+        }
+    }
+
+    if($erreur == true)
+    {
+        ///////////// TWIG //////////////
+        $template = $twig->loadTemplate('futur_etudiant/bon_choix/index.html.twig');
+        echo $template->render(array(
+            'niveau' => $strNiveau,
+            'arrMenuLiensActifs' => $arrMenuActif,
+            'page' => "Fais-tu le bon choix? | ",
+            'xml' => $arrQuestions,
+            'tId' => $arrId,
+            'erreur' => "Veuillez compléter toutes les questions.",
+            'fichier_script' => 'quiz2.js'
+        ));
+    }
+    else
+    {
+        $arrReponses = array();
+        for($cpt = 0; $cpt <= 4; $cpt++)
+        {
+            if($_GET['Q'.$cpt] == "Oui")
+            {
+                $arrReponses[$cpt]['libelleReponse']= $arrQuestions[$cpt]['libelleReponse'][0];
+                $arrReponses[$cpt]['texteReponse'] = $arrQuestions[$cpt]['texteReponse'][0];
+            }
+            else
+            {
+                $arrReponses[$cpt]['libelleReponse'] = $arrQuestions[$cpt]['libelleReponse'][1];
+                $arrReponses[$cpt]['texteReponse'] = $arrQuestions[$cpt]['texteReponse'][1];
+            }
+        }
+        ///////////// TWIG //////////////
+        $template = $twig->loadTemplate('futur_etudiant/bon_choix/resultat.html.twig');
+        echo $template->render(array(
+            'niveau' => $strNiveau,
+            'arrMenuLiensActifs' => $arrMenuActif,
+            'page' => "Fais-tu le bon choix? | ",
+            'xml' => $arrQuestions,
+            'reponses' => $arrReponses,
+            'tId' => $arrId
+        ));
+    }
+}
+else
+{
+    ///////////// TWIG //////////////
+    $template = $twig->loadTemplate('futur_etudiant/bon_choix/index.html.twig');
+    echo $template->render(array(
+        'niveau' => $strNiveau,
+        'arrMenuLiensActifs' => $arrMenuActif,
+        'page' => "Fais-tu le bon choix? | ",
+        'xml' => $arrQuestions,
+        'tId' => $arrId,
+        'fichier_script' => 'quiz2.js'
+    ));
+}
