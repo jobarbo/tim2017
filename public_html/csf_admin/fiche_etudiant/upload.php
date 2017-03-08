32055e24749061b814c5061d7f1f9b18695c9861
Annabelle Violette <anna.violette@hotmail.com>
2017-03-08 12:49:40 -0500

    A - fiche etudiant admin

--- public_html/csf_admin/fiche_etudiant/upload.php
+++ public_html/csf_admin/fiche_etudiant/upload.php
@@ -5,27 +5,21 @@
  * Date: 17-02-28
  * Time: 11:56
  */
-
 $intMatricule = $_POST['matricule'];
-
 include("../inc/classes/uploader.php");
-
 $uploader = new Uploader();
 $uploader->setDir('../../public/dist/images/diplomes/');                       // Dossier de téléversement
 $uploader->setExtensions(array('png'));                      // Extensions permises
 $uploader->sameName = true;                                 // Conserve le même nom de fichier
 $uploader->setMaxSize(5);                                   // Taille max en Mo
-
 if($uploader->uploadFile('photoEtudiant'))                  // Attribut name du formulaire
 {
     $imageName = $uploader->getUploadName();                // Retourne le nom du fichier téléversé
-
     header('Location: index.php?id=' . $intMatricule);
 }
 else
 {                                                      // Échec lors du téléversement
     $message = $uploader->getMessage();                // Message d'erreur
-
     var_dump($message);
 }
 ?>
