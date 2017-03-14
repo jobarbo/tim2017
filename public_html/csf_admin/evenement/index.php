07d4258fbbea80f5972115f96a28ca8f515d6708
Jonathan Barbeau <Barbeau.jonathan@gmail.com>
2017-03-14 11:41:44 -0400

    Finission admin

--- public_html/csf_admin/evenement/index.php
+++ public_html/csf_admin/evenement/index.php
@@ -5,30 +5,70 @@ $strNiveauAdmin = "../../";
 $strSection = "Ajout d'évenement";
 setlocale(LC_TIME,"fr_CA");
 
+$today_date = date("Y-m-d");
+//var_dump("Today is " . date("Y-m-d") . "<br>");
+$date_formater = strftime("%A %e %B", strtotime($today_date));
+
+if (isset($_GET['errorDate'])){
+    echo 'vous ne pouvez publié un evenement qui se produira dans plus de 15 jours.';
+}
+
+if (isset($_GET['submit_actu'])) {
+
+function check_in_range($start_date, $end_date, $date_from_user)
+{
+  // Convert to timestamp
+  $start_ts = strtotime($start_date);
+  $end_ts = strtotime($end_date);
+  $user_ts = strtotime($date_from_user);
+
+  // Check that user date is between start & end
+  return (($user_ts >= $start_ts) && ($user_ts <= $end_ts));
+}
+
+    $exp_actu = strtotime($_GET['exp_actu']);
+    $end_date = $_GET['exp_actu'];
+    $start_date = date('Y-m-d', strtotime('-15 days', $exp_actu));
+    $date_from_user =  $_GET['date_ajd'];
+
+
+
+}
+
+
+
+
+
 /*************** 2. INSTANCIATION CONFIG ET TWIG ***********************/
 require_once($strNiveau . 'inc/scripts/fctcommunes.inc.php');
 
 
 if (isset($_GET['submit_actu'])) {
-    try{
-        $strSQLInsertEvenement = "INSERT INTO t_evenement (titre_actualite, description_actualite, url_actualite)
-        VALUES (". "'".$_GET['titre_actu']."'"."," ."'".$_GET['desc_actu']."'".","."'".$_GET['url_actu']."'".");";
-        //var_dump($strSQLUpdateEvenement);
-        
-        if($objConnMySQLi->query($strSQLInsertEvenement) === TRUE){
-            $texteErreurInsert = "";
-            header('Location:'. $strNiveau);
-        }
-        else{
-            $strMsgErrInsert= "<p>L'ajout n'a pu être éffectué, réessayez plus tard</p>";
-            $except = new Exception($strMsgErrInsert);
+    if(check_in_range($start_date, $end_date, $date_from_user) == 1){
+        try{
+            $strSQLInsertEvenement = "INSERT INTO t_evenement (titre_actualite, description_actualite, url_actualite,date_publication,date_expiration)
+            VALUES (". "'".$_GET['titre_actu']."'"."," ."'".$_GET['desc_actu']."'".","."'".$_GET['url_actu']."'".","."'".$_GET['date_ajd']."'".","."'".$_GET['exp_actu']."'".");";
+            //var_dump($strSQLInsertEvenement);
             
-            throw $except;
+            if($objConnMySQLi->query($strSQLInsertEvenement) === TRUE){
+                $texteErreurInsert = "";
+                header('Location:'. $strNiveau . 'administration.php');
+            }
+            else{
+                $strMsgErrInsert= "<p>L'ajout n'a pu être éffectué, réessayez plus tard</p>";
+                $except = new Exception($strMsgErrInsert);
+                
+                throw $except;
+            }
+        }
+        catch (Exception $e) {
+            $texteErreurInsert = $e->getMessage();
         }
     }
-    catch (Exception $e) {
-        $texteErreurInsert = $e->getMessage();
+    else{
+        header('Location:'. 'index.php?ajout&errorDate="true"');
     }
+    
 }
 
 
@@ -44,7 +84,7 @@ if (isset($_GET['modif_actu'])){
         WHERE id_actualite = ' . $_GET['id_actu'] . '';
         if($objConnMySQLi->query($strSQLUpdateEvenement)=== TRUE){
             $texteErreurUpdate = "";
-             header('Location:'. $strNiveau);
+             header('Location:'. $strNiveau . 'administration.php');
         }
         else{
             $strMsgErrUpdate= "<p>Les modifications n'ont pu être apportées, réessayez plus tard</p>";
@@ -76,7 +116,8 @@ if (isset($_GET['edit'])){
                 'date_publication'=> $objLigneFetchEvenement->date_publication,
                 'date_expiration'=> $objLigneFetchEvenement->date_expiration,
                 'url_actualite'=>$objLigneFetchEvenement->url_actualite,
-                'image_actualite'=> $objLigneFetchEvenement->image_actualite
+                'image_actualite'=> $objLigneFetchEvenement->image_actualite,
+                'id'=> $_GET['id_actu']
                 );
             }
             $strMsgErrNouvelle =false;
@@ -93,6 +134,7 @@ if (isset($_GET['edit'])){
     
 }
 
+/*********** Truc pour les dates *************/
 
 
 
@@ -108,7 +150,9 @@ if (isset($_GET['ajout'])){
     'title' => "Section administrative | TIM",
     'page' => "",
     'niveau' => $strNiveau,
-    'niveauAdmin' => $strNiveauAdmin
+    'niveauAdmin' => $strNiveauAdmin,
+    'date_ajd' => $today_date
+    
     
     ));
 }
@@ -121,6 +165,7 @@ if (isset($_GET['edit'])){
     'niveau' => $strNiveau,
     'niveauAdmin' => $strNiveauAdmin,
     'arrEditNouvelle' => $arrEditNouvelle
+
     
     ));
 }
